570ded723d3be9f4cc110e6235fc11de35d29867
DrewMcD25 <drewmcd25@yahoo.com>
2021-08-14 16:04:30 -0500

    things we worked on saturday

---
+++ src/components/lilo.js
@@ -0,0 +1,18 @@
+import react from "react"
+import reactDom from "react-dom"
+
+const login = (username, password) => {
+    return (
+        <div>
+            <input type = "text" placeholder = "username">username</input>
+            <input type = "password" placeholder = "password">password</input>
+            <button>Log In</button>
+        </div>
+    )
+
+    
+}
+
+
+
+export default lilo;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 import ReactDOM from 'react-dom';
-import react from react
+import react from 'react';
+import lilo from './components/lilo';
+
+const App = ()=>{
+    return (
+        < >
+        <div>
+            <login/>
+        </div>
+        
+        </>
+    )
+}
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
