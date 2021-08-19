1e413e14c4ff47d1807e85e9a2d13475f2fe818d
Clayton <claytonKcottrell@Outlook.com>
2021-08-18 21:07:15 -0500

    changes for login

--- src/components/lilo.js
+++ src/components/lilo.js
@@ -1,14 +1,32 @@
-import React from 'react'
+import React , { useState } from 'react';
 import ReactDOM from 'react-dom';
 
 const Login = ({userToken}) => {
+    function handleChange(event) {
+     const userKey = event.target.attributes['name'].value   
+     const newState = {...userName}
+     newState[userKey] = event.target.value
+     setUser(newState) 
+    }
     const [user, setUser] = useState({username: '', password: ''})
-
     async function saveToken() {
         try {
-            const data = await API.makeRequest({'/lilo' , 'posts', user})
-            localstorage.setItem('user-token', data.token);
-            makeToken(data.token);
+           // const data = await API.makeRequest({'lilo','posts', user})
+           const response = await fetch('https://strangers-things.herokuapp.com/api/2105-OKU-RM-WEB-PT/users/login', {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    user: {
+                    username: '',
+                    password: ''
+                    }
+                })
+            })
+            const data = await response.json()
+            localstorage.setItemuser-token('', data.token);
+            (data.token);
         } catch(error) {
             alert(error)
         }
@@ -16,7 +34,7 @@ const Login = ({userToken}) => {
     return (
         <div>
             <form onSubmit = {(event) => {} }>  
-            <input type = "text" required name = "username" placeholder = "username"></input>
+            <input type = "text" onChange = {handleChange} required name = "username" placeholder = "username"></input>
             <input type = "password" required name = "password" placeholder = "password"></input>
             <button>Log In</button>
             </form>
---
+++ src/components/utilites/token.js
@@ -0,0 +1,15 @@
+TOKEN_KEY = 'user-token';
+
+export default {
+    grabToken: function() {
+        return localStorage.getItem(TOKEN_KEY);
+    },
+
+    setToken: function(token) {
+        localStorage.setItem(TOKEN_KEY, token);
+    },
+
+    removeToken: function() {
+        localstorage.removeItem(TOKEN_KEY);
+    }
+}
