927ee16bd18a3773ef8881882a6b08d3eb72b7c5
DrewMcD25 <drewmcd25@yahoo.com>
2021-08-19 21:16:15 -0500

    started nav bar

---
+++ src/components/posts.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom"
+
+const Posts = () => {
+    return (
+        <div>
+            <h1>POSTS</h1>
+        </div>
+    )
+}
+
+export default Posts
--- src/index.js
+++ src/index.js
@@ -2,30 +2,39 @@ import ReactDOM from 'react-dom';
 import React, { useState } from 'react';
 import Login from './components/lilo';
 import Home from './components/home'
-import {BrowserRouter as Router, Route, Switch, Link }from "react-router-dom"
+import Posts from './components/posts'
+import Profile from './components/profile'
+import Navbar from './components/navbar';
+import { BrowserRouter as Router, Route, Switch, Link, Redirect } from "react-router-dom"
 <link href="/public/style.css"></link>
 
-const App = ()=>{
+const App = () => {
     const [isNewUser, setIsNewUser] = useState(true)
     const renderAuthForm = (e) => {
         if (isNewUser) {
-            return <Login toggleNewUser={setIsNewUser}/>
+            return <Login toggleNewUser={setIsNewUser} />
         } else {
             return <Register toggleNewUser />
         }
     }
     return (
         <>
-        <Router>
-            <div>
-            <Route exact path = "/login"><Login/></Route>
-            {/* <Route exact path = "/posts"><Posts/></Route>
-            <Route exact path = "/profile"><Profile/></Route> */}
-            <Route exact path = "/"><Home/></Route>
-            </div>
-        </Router>
-        
-        
+            <Router>
+                <Navbar/>
+                <main>
+                    <switch>
+                        <div>
+                            <Route exact path="/login"><Login /></Route>
+                            <Route exact path="/posts"><Posts /></Route>
+                            <Route exact path="/profile"><Profile /></Route>
+                            <Route exact path="/"><Home /></Route>
+                        </div>
+                    </switch>
+                </main>
+            </Router>
+
+
+
         </>
     )
 }
