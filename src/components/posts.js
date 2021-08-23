9e03d947a79f8904f57aced93013271abc0e7e0a
DrewMcD25 <drewmcd25@yahoo.com>
2021-08-22 21:02:08 -0500

    changes

--- src/components/posts.js
+++ src/components/posts.js
@@ -10,31 +10,32 @@ import { getPosts } from "./utilites/api";
 //     console.log(response)
 // }
 
-const Posts = ({postList, setPostList,}) => {
-    useEffect( async function () {
+const Posts = ({ postList, setPostList, }) => {
+    useEffect(async function () {
         fetch('https://strangers-things.herokuapp.com/api/2105-OKU-RM-WEB-PT/posts')
-  .then(response => response.json())
-  .then(result => { setPostList(result.data.posts)
-    console.log(result);
-  })
-  .catch(console.error);
+            .then(response => response.json())
+            .then(result => {
+                setPostList(result.data.posts)
+                console.log(result);
+            })
+            .catch(console.error);
 
         // const postAppear = await getPosts()
-        
+
         // console.log(postAppear)
     }, []);
     console.log(postList);
-const postElement = postList.map((post) => 
-    <div id= "listOfPosts"> 
-        <h1>Name: {post.author.username}</h1>  
-        <h1>Title: {post.title}</h1>
-        <h2>Description: {post.description}</h2>
-        <h3>Location: {post.location}</h3>
-        <h3>Price: {post.price}</h3>
-    </div>
+    const postElement = postList.map((post) =>
+        <div id="listOfPosts">
+            <h1>Name: {post.author.username}</h1>
+            <h1>Title: {post.title}</h1>
+            <h2>Description: {post.description}</h2>
+            <h3>Location: {post.location}</h3>
+            <h3>Price: {post.price}</h3>
+        </div>
 
 
-                                                );
+    );
 
     return (
         <div id="posts">
--- src/index.js
+++ src/index.js
@@ -19,6 +19,7 @@ const App = () => {
         } else {
             return <Register toggleNewUser />
         }
+        
     }
     
     return (
